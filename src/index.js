6a3e0b02e6c4c0e873cb89d5694e2681beeeb03d
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-08-01 23:31:40 +0300

    add fetch and debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box')
+
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
+
+function onInput(evt){
+    const queryValue = evt.target.value.trim()
+    console.log(queryValue);
+
+    fetch(`https://restcountries.com/v3.1/name/${queryValue}?fields=name,capital,population,flags,languages`)
+    .then(responce => responce.json())
+    .then(data => {
+        console.log(data);
+        // console.log(data[0].capital);
+    })
+}
+
