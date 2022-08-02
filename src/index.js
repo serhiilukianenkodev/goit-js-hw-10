022456285dfbadecd534662fdcd35dc39915ca17
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-08-02 23:33:52 +0300

    show countries func

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import {fetchCountries} from './js/fetchCountries.js';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
 
-const inputEl = document.querySelector('#search-box')
+const refs = {
+    inputEl: document.querySelector('#search-box'),
+    countryListEl: document.querySelector('.country-list'),
+    countryInfoEl: document.querySelector('.country-info')
+}
+// const inputEl = document.querySelector('#search-box')
 
-inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
+refs.inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
 
 function onInput(evt){
     const queryValue = evt.target.value.trim()
@@ -14,7 +19,20 @@ function onInput(evt){
 
     fetchCountries(queryValue)
     .then(data => {
-        console.log(data);
+        // console.log(data);
+        showCountriesList(data);
         // console.log(data[0].capital);
     })
 }
+
+function showCountriesList(data){
+    console.log(data);
+    const markup = data.map(({flags, name}) => {
+        console.log(flags.svg, name.official);
+        return `<li class = "country-item"><img src="${flags.svg}" alt="flag of ${name.official}" class = "country-img">${name.official}</li>`
+    }).join('')
+
+    refs.countryListEl.innerHTML= markup;
+}
+
+// function 
