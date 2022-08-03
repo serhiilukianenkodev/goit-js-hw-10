27aa9e8e43409013d0aaeec6a6b28d829861ad54
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-08-03 22:38:13 +0300

    preload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import {fetchCountries} from './js/fetchCountries.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 1000;
 
@@ -10,29 +11,63 @@ const refs = {
     countryInfoEl: document.querySelector('.country-info')
 }
 // const inputEl = document.querySelector('#search-box')
+// console.log(refs.countryInfoEl);
 
 refs.inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
 
 function onInput(evt){
     const queryValue = evt.target.value.trim()
-    console.log(queryValue);
+    // console.log(queryValue);
 
     fetchCountries(queryValue)
     .then(data => {
-        // console.log(data);
+       
+        if (data.length === 1) {
+            refs.countryListEl.innerHTML = '';
+            showCountry(data[0])
+            return
+        }
+
+        if (data.length > 10) {
+            Notify.info('Too many matches found. Please enter a more specific name.');
+            refs.countryListEl.innerHTML = '';
+            refs.countryInfoEl.innerHTML = '';         
+            return}
+
+        refs.countryInfoEl.innerHTML = '';
         showCountriesList(data);
-        // console.log(data[0].capital);
-    })
+}).catch(error => {
+    Notify.failure('Oops, there is no country with that name')
+    refs.countryListEl.innerHTML = '';
+    refs.countryInfoEl.innerHTML = '';
+})
 }
 
+
+
 function showCountriesList(data){
-    console.log(data);
+    // console.log(data);
     const markup = data.map(({flags, name}) => {
-        console.log(flags.svg, name.official);
+        // console.log(flags.svg, name.official);
         return `<li class = "country-item"><img src="${flags.svg}" alt="flag of ${name.official}" class = "country-img">${name.official}</li>`
     }).join('')
 
     refs.countryListEl.innerHTML= markup;
 }
 
-// function 
+function showCountry(country){
+    // console.log(country);
+    const {flags, name, capital, languages, population} = country
+    // console.log(flags.svg, name.official, capital, languages, population);
+    const markup = `
+    <div class = "country-about">
+        <img src="${flags.svg}" alt="flag of ${name.official}" class = "country-img">
+        <p class="country">${name.official}</p>
+    </div>
+    <p><span>Capital: </span>${capital}</p>
+    <p><span>Population: </span>${population}</p>
+    <p><span>Languages: </span>${Object.values(languages).join(', ')}</p>`;
+// console.log(markup);
+refs.countryInfoEl.innerHTML = markup;
+// console.log(refs.countryInfoEl);
+}
